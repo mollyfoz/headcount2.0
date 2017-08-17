468e8362b6d13e4d683c486a7a540db85b4d1b40
Nik Bornemeier <NikolausRB@gmail.com>
2017-08-17 14:28:36 -0600

    create district average functionality, WIP

--- src/helper.js
+++ src/helper.js
@@ -28,13 +28,51 @@ export default class DistrictRepository {
     }
   }
 
+  districtAverage(arg) {
+    let district = this.findByName(arg)
+    let districtArray = Object.values(district.data)
+    let counter = districtArray.length
+    let districtAVG = districtArray.reduce((sum, value)=>{
+      sum = (sum + value)/counter
+      sum = Math.round(1000 * value)/1000
+      // console.log("sumVAL: ",sum+value)
+      return sum
+    }, 0);
+    // console.log('districtArray: ', districtArray)
+    // console.log('counter: ',counter)
+    // console.log('distAVG: ', districtAVG);
+    return districtAVG;
+
+  }
+
+  compareDistrictAverages(arg1, arg2) {
+    let districtOneAverage = this.districtAverage(arg1);
+    let districtTwoAverage = this.districtAverage(arg2);
+    // console.log("dist1AVG: ", districtOneAverage)
+    // console.log("dist2AVG: ", districtTwoAverage)
+    // console.log('distsAvg: ', districtOneAverage / districtTwoAverage)
+    let districtAverages = districtOneAverage + districtTwoAverage / 2;
+    districtAverages = Math.round(1000 * districtAverages)/1000
+    console.log('avg: ', districtAverages)
+    return
+
+    // console.log(Object.values(districtOne.data));
+    // console.log(Object.values(districtTwo.data))
+  }
+
   findAllMatches(name) {
     let allData = Object.keys(this.data);
     let matches = allData.map(location => this.data[location])
 
       if(name) {
+        this.compareDistrictAverages('Academy 20', 'Colorado')
+        // this.compareDistrictAverages("Colorado", "Academy 20")
         return matches.filter(distName => distName.location.includes(name.toUpperCase()))
       }
     return matches
   }
+
+
+
+
 }

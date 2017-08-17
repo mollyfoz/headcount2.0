5b8c3d26f2495a3839c4c0acd48e97921729be81
Nik Bornemeier <NikolausRB@gmail.com>
2017-08-17 17:13:55 -0600

    finish compare functionality, tests are not passing

--- src/helper.js
+++ src/helper.js
@@ -33,31 +33,24 @@ export default class DistrictRepository {
     let districtArray = Object.values(district.data)
     let counter = districtArray.length
     let districtAVG = districtArray.reduce((sum, value)=>{
-      sum = (sum + value)/counter
-      sum = Math.round(1000 * value)/1000
-      // console.log("sumVAL: ",sum+value)
+
+      sum = sum + value
       return sum
     }, 0);
-    // console.log('districtArray: ', districtArray)
-    // console.log('counter: ',counter)
-    // console.log('distAVG: ', districtAVG);
-    return districtAVG;
+      districtAVG = districtAVG/counter
+    return Math.round(1000 * districtAVG)/1000
 
   }
 
   compareDistrictAverages(arg1, arg2) {
     let districtOneAverage = this.districtAverage(arg1);
     let districtTwoAverage = this.districtAverage(arg2);
-    // console.log("dist1AVG: ", districtOneAverage)
-    // console.log("dist2AVG: ", districtTwoAverage)
-    // console.log('distsAvg: ', districtOneAverage / districtTwoAverage)
-    let districtAverages = districtOneAverage + districtTwoAverage / 2;
+    console.log("dist1AVG: ", districtOneAverage)
+    console.log("dist2AVG: ", districtTwoAverage)
+    let districtAverages = (districtOneAverage / districtTwoAverage);
     districtAverages = Math.round(1000 * districtAverages)/1000
     console.log('avg: ', districtAverages)
-    return
-
-    // console.log(Object.values(districtOne.data));
-    // console.log(Object.values(districtTwo.data))
+    return districtAverages
   }
 
   findAllMatches(name) {
@@ -65,7 +58,7 @@ export default class DistrictRepository {
     let matches = allData.map(location => this.data[location])
 
       if(name) {
-        this.compareDistrictAverages('Academy 20', 'Colorado')
+        this.compareDistrictAverages(arg1, arg2)
         // this.compareDistrictAverages("Colorado", "Academy 20")
         return matches.filter(distName => distName.location.includes(name.toUpperCase()))
       }

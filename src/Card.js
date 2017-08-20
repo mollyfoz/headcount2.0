1cda1006c87ded874b8e7909d6cc5358e29f665b
Molly Foster <mollyrfoster@gmail.com>
2017-08-20 16:41:24 -0600

    fixed key issue

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 export default class Card extends Component {
   constructor() {
     super();
       this.selectCard = this.selectCard.bind(this)
       this.state = {
-        selected: false,
+        selected: false
       }
   }
 
@@ -14,7 +14,6 @@ export default class Card extends Component {
     if (this.state.selected === false) {
       this.setState({ selected: true } )
       this.props.addCompare(this)
-
     } else {
       this.setState({ selected: false } )
       this.props.addCompare(this)
@@ -31,9 +30,8 @@ export default class Card extends Component {
   }
 
   render() {
-
-   const { location, data } = this.props
-   const keys = Object.keys(data)
+   const { id, location, data } = this.props
+   const districtNames = Object.keys(data)
 
    let highScore = {
      color: '#006666'
@@ -43,29 +41,23 @@ export default class Card extends Component {
      color: '#FF803E'
    }
 
-
-    let keyVals = keys.map((key, i) => {
-      if (data[key] >= 0.5) {
-        return <li style={ highScore } className='year-data' key={ i }> { key }: { data[key] } </li>
+    let nameVals = districtNames.map((name, i) => {
+      if (data[name] >= 0.5) {
+        return <li style={ highScore } className='year-data' key={ i }> { name }: { data[name] } </li>
       } else {
-        return <li style={ lowScore } className='year-data' key={ i }> { key }: { data[key] } </li>
+        return <li style={ lowScore } className='year-data' key={ i }> { name }: { data[name] } </li>
       }
     })
 
     return (
-      <div ref={element => this.div = element} className='data-card' onClick={this.selectCard.bind(this)}>
+      <div key={ id } ref={ element => this.div = element } className='data-card' onClick={ this.selectCard.bind(this) }>
         <h3>{ location }</h3>
         <div className='data-list'>
           <ul>
-            { keyVals }
+            { nameVals }
           </ul>
         </div>
       </div>
     )
   }
-}
-
-Card.propTypes = {
-  location: PropTypes.string.isRequired,
-  data: PropTypes.object.isRequired
 }
